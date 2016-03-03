1666c852aff7b2e852b1fe00d2aa9f07386d5ebb
yoyonel <yoyonel@hotmail.com>
2016-03-03 21:46:24 +0100

    Thread - Producer/Consumer - Rajout d'une gestion des interruptions pour finaliser l'écriture du fichier log (example.log)

--- threads/waitfree_producer_consumer/app.cpp
+++ threads/waitfree_producer_consumer/app.cpp
@@ -3,30 +3,45 @@
 #include <boost/thread/thread.hpp>
 #include <boost/lockfree/spsc_queue.hpp>
 #include <boost/atomic.hpp>
+
 #include <iostream>
 #include <cstdlib>
-
-typedef int Image;
+#include <signal.h>
 
 
 // Sources, URLs:
 // - http://www.boost.org/doc/libs/1_54_0/doc/html/lockfree/examples.html#lockfree.examples.waitfree_single_producer_single_consumer_queue
 
-// Note: ce n'est pas ce que souhaite faire Julien !
-// Dans ce pattern, le consommateur (ou l'action de consommer) bloque la production
 
+//typedef int Image;
+struct Image {
+    void set_id(const uint& _id) { m_id = _id; }
+    const uint& id() const { return m_id; }
+
+    uint m_id;
+    //char* ptr_img;
+};
+
+
+const uint capacity_queue = 5;
+boost::lockfree::spsc_queue<Image, boost::lockfree::capacity<capacity_queue> > spsc_queue;
+
+// Scénario (normalement) représentant les temps d'acquisition/écriture dans le Kangoo
 uint time_transfert_usb3 = 250;
 uint time_transfert_disk = 100;
+// Scénario avec des temps d'acquisition inversés pour provoquer des overflows
+//const uint time_transfert_usb3 = 10;
+//const uint time_transfert_disk = 250;
 
 struct Capture
 {
     // Capture une image LB
     void take()
     {
-        // 50 ms pour prendre une image
-        boost::this_thread::sleep(boost::posix_time::milliseconds(76));
-        // 
-        img = std::rand();
+        // TEMPOrisaton pour le transfert USB3->RAM
+        boost::this_thread::sleep(boost::posix_time::milliseconds(time_transfert_usb3));
+        // on assigne un ID à l'image (pour la répérer dans les logs)
+        img.set_id(std::rand());
     }
 
     const Image& get() const { return img; }
@@ -49,8 +64,6 @@ struct WrapperLadyBug
 };
 typedef boost::shared_ptr<WrapperLadyBug> WrapperLadyBugPtr;
 
-boost::lockfree::spsc_queue<Image, boost::lockfree::capacity<1024> > spsc_queue;
-
 // PRODUCER
 void runLadyBug(WrapperLadyBugPtr sptr_wLB)
 {
@@ -61,39 +74,68 @@ void runLadyBug(WrapperLadyBugPtr sptr_wLB)
         const Image& img = value.get();
         while (!spsc_queue.push(img))
                 ;
-        INFO << "[PRODUCER] Transfert USB->RAM Image n° " << img;
+        INFO << "[PRODUCER] Transfert USB->RAM Image n° " << img.id();
     }
 }
 
 boost::atomic<bool> done (false);
 
+// Function d'écriture de l'image
+// RAM -> DISK
+bool write_img_to_disk(const Image& _img) 
+{
+    // temps pour écrire
+    boost::this_thread::sleep(boost::posix_time::milliseconds(time_transfert_disk));
+    INFO << "[CONSUMER] Transfert RAM->DISK Image n° " << _img.id();
+    return true;
+}
+
 // CONSUMER
 void runwriteLadyBug (WrapperLadyBugPtr sptr_wLB) 
 {
     while(true) {
-        Image value;
+        Image img;
 
-        /**
         while (!done) {
-            while (spsc_queue.pop(value)) {
-                // temps pour écrire
-                boost::this_thread::sleep(boost::posix_time::milliseconds(75));
-                //
-                INFO << "[CONSUMER] Transfert RAM->DISK Image n° " << value;
-            }
+            while (spsc_queue.pop(img))
+                write_img_to_disk(img);
         }
-        /**/
-        while (spsc_queue.pop(value)) {
-            // temps pour écrire
-            boost::this_thread::sleep(boost::posix_time::milliseconds(75));
-            INFO << "[CONSUMER] Transfert RAM->DISK Image n° " << value;
-        }
+
+        while (spsc_queue.pop(img))
+            write_img_to_disk(img);
     }
+}
+
+void
+termination_handler (int signum)
+{
+  exit(1);
+}
+
+void setup_interruption_handling()
+{
+    struct sigaction new_action, old_action;
+    
+    /* Set up the structure to specify the new action. */
+    new_action.sa_handler = termination_handler;
+    sigemptyset (&new_action.sa_mask);
+    new_action.sa_flags = 0;
+
+    sigaction (SIGINT, NULL, &old_action);
+    if (old_action.sa_handler != SIG_IGN)
+    sigaction (SIGINT, &new_action, NULL);
+    sigaction (SIGHUP, NULL, &old_action);
+    if (old_action.sa_handler != SIG_IGN)
+    sigaction (SIGHUP, &new_action, NULL);
+    sigaction (SIGTERM, NULL, &old_action);
+    if (old_action.sa_handler != SIG_IGN)
+    sigaction (SIGTERM, &new_action, NULL);
 }
 
 // INSTANCIATIONS
 // Wrapper LadyBug
 WrapperLadyBug *m_Wrapper = new WrapperLadyBug();
+
 
 int main(int argc, char* argv[])
 {
@@ -113,6 +155,9 @@ int main(int argc, char* argv[])
     done = true;
     writeThreadLadyBug.detach();
 
+    setup_interruption_handling();    
+
+    // boucle infinie d'exécution
     while(true) {
     }
 }
